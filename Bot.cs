3141624a37c9ae93a884e4d9102c379afcd61ad8
Hugo Bellemare-Vallieres <hbvallieres@ova.ai>
2022-11-15 18:46:10 -0500

    Something

--- Bot.cs
+++ Bot.cs
@@ -6,6 +6,9 @@ namespace Application
     public class Bot
     {
         public const string NAME = "MyBot C♭";
+
+        Node TargetNode;
+
         private static readonly List<string> _directions = new List<string>
         {
             "N", "NE", "E", "SE", "S", "SW", "W", "NW"
@@ -23,13 +26,13 @@ namespace Application
         {
             if(tick.CurrentLocation == null)
             {
+                Setup(tick);
                 return new Move { Kind = MoveKind.Spawn, Position = FindWhereToSpawn(tick)};
             }
             else
             {
                 Position closestPort = FindClosestPort(tick);
-                Console.WriteLine("Current tick: " + tick.CurrentTick);
-                Console.WriteLine($"x : {closestPort.Column} et y : {closestPort.Row}");
+                Console.WriteLine($"Target => x : {closestPort.Column} et y : {closestPort.Row}");
                 return FindNextAction(tick.CurrentLocation, closestPort);
             }
         }
@@ -50,7 +53,7 @@ namespace Application
             foreach(var indexPort in portsVisites)
                 ports.RemoveAt(indexPort);
 
-            Console.WriteLine($"x : {tick.CurrentLocation.Column} et y : {tick.CurrentLocation.Row}");
+            Console.WriteLine($"Current Location => x : {tick.CurrentLocation.Column} et y : {tick.CurrentLocation.Row}");
 
             foreach(var direction in ports)
             {
@@ -70,7 +73,168 @@ namespace Application
             return tick.Map.Ports[0];
         }
 
+        private List<Position> CompilePath(Node startNode)
+        {
+            List<Position> path = new List<Position>();
+            bool success = AStarSearch(startNode);
+            if(success)
+            {
+                Node node = TargetNode;
+                while (node.ParentNode != null)
+                {
+                    path.Add(node.Position);
+                    node = node.ParentNode;
+                }
+                path.Reverse();
+            }
+            return path;
+        }
+
+        public void Setup(Tick tick)
+        {
+            BotState.Nodes = new List<List<Node>>();
+            for (int i = 0; i < tick.Map.Topology.Count; i++)
+            {
+                List<Node> nodeColumn = new List<Node>();
+                for (int j = 0; j < tick.Map.Topology[i].Count; j++)
+                {
+                    if (tick.Map.Topology[i][j] > tick.Map.TideLevels.Min)
+                    {
+                        // Ces cases seront PARFOIS non navigables
+                        Console.WriteLine("nodecolumn" + nodeColumn.Count);
+                        Node newNode = new (i, j, false);
+                        Console.WriteLine(newNode);
+                        nodeColumn.Add(newNode);
+                    }
+                    else
+                    {
+                        // Ces cases seront TOUJOURS navigables
+
+
+                        Console.WriteLine("nodecolumn" +nodeColumn.Count);
+                        Console.WriteLine("i" + i);
+                        Console.WriteLine("j" + j);
+
+
+                        Node newNode = new (i, j, true);
+
+
+                        Console.WriteLine(newNode);
+                        nodeColumn.Add(newNode);
+                    }
+                }
+                BotState.Nodes.Add(nodeColumn);
+            }
+        }
+
         public string FindNextMove(Position currentPosition, Position target)
+        {
+            TargetNode = BotState.Nodes[target.Column][target.Row];
+            List<Position> path = CompilePath(BotState.Nodes[currentPosition.Column][currentPosition.Row]);
+            if(path.Count == 0)
+            {
+                Console.WriteLine("path nul");
+                return "N";
+            }
+            return FindStringDirection(currentPosition, path[0]);
+
+        }
+
+        private List<Position> GetAdjacentPositions(Position position)
+        {
+            List<Position> result = new List<Position>();
+
+            for (int i = -1; i <= 1; i++)
+            {
+                Position adjacentPosition = new Position();
+                adjacentPosition.Row = position.Row -1;
+                adjacentPosition.Column = position.Column + i;
+                result.Add(new Position());
+            }
+
+            for (int i = -1; i <= 1; i+=2)
+            {
+                Position adjacentPosition = new Position();
+                adjacentPosition.Row = position.Row;
+                adjacentPosition.Column = position.Column + i;
+                result.Add(new Position());
+            }
+
+            for (int i = -1; i <= 1; i++)
+            {
+                Position adjacentPosition = new Position();
+                adjacentPosition.Row = position.Row + 1;
+                adjacentPosition.Column = position.Column + i;
+                result.Add(new Position());
+            }
+
+            return result;
+        }
+
+        private List <Node> GetAdjacentSailableNodes (Node fromNode)
+        {
+            List<Node> sailableNodes = new List<Node>();
+            IEnumerable<Position> nextPositions = GetAdjacentPositions(fromNode.Position);
+
+            foreach (Position position in nextPositions)
+            {
+                int x = position.Column;
+                int y = position.Row;
+
+                if (x < 0 || x >= BotState.Nodes.Count || y < 0 || y >= BotState.Nodes[0].Count)
+                    continue;
+
+                Node node = BotState.Nodes[x][y];
+                node.H = Node.GetTraversalCost(node.Position, TargetNode.Position);
+
+                if (!node.IsSailable)
+                    continue;
+
+                if (node.State == NodeState.Closed)
+                    continue;
+
+                if(node.State == NodeState.Open)
+                {
+                    float traversalCost = Node.GetTraversalCost(node.Position, fromNode.Position);
+                    float gTemp = fromNode.G + traversalCost;
+                    if (gTemp < node.G)
+                    {
+                        node.G = gTemp;
+                        node.ParentNode = fromNode;
+                        sailableNodes.Add(node);
+                    }
+                }
+                else
+                {
+                    node.ParentNode = fromNode;
+                    node.State = NodeState.Open;
+                    sailableNodes.Add(node);
+                }
+            }
+            return sailableNodes;
+        }
+
+        private bool AStarSearch(Node currentNode)
+        {
+            currentNode.State = NodeState.Closed;
+            List<Node> nextNodes = GetAdjacentSailableNodes(currentNode);
+            nextNodes.Sort((node1, node2) => node1.F.CompareTo(node2.F));
+            foreach (Node nextNode in nextNodes)
+            {
+                if(nextNode.Position.Row == TargetNode.Position.Row && nextNode.Position.Column == TargetNode.Position.Column)
+                {
+                    return true;
+                }
+                else
+                {
+                    if (AStarSearch(nextNode))
+                        return true;
+                }
+            }
+            return false;
+        }
+
+        public string FindStringDirection(Position currentPosition, Position target)
         {
            if(currentPosition.Row < target.Row && currentPosition.Column < target.Column)
            {
---
+++ Node.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public class Node
+    {
+        public Position Position { get; set; }
+        public bool IsSailable { get; set; }
+        public float G { get; set; }
+        public float H { get; set; }
+        public float F { get { return G + H; } }
+        public NodeState State { get; set; }
+        public Node ParentNode { get; set; }
+
+        public static float GetTraversalCost(Position from, Position to)
+        {
+            return (float)Math.Sqrt(Math.Pow(Math.Abs(from.Column - to.Column), 2) + Math.Pow(Math.Abs(from.Row - to.Row), 2));
+        }
+
+        public Node(int x, int y, bool isSailable)
+        {
+            Console.WriteLine("Inside node constructor");
+            Position.Column = x;
+            Position.Row = y;
+            IsSailable = isSailable;
+            G = int.MaxValue;
+            H = int.MaxValue;
+            State = NodeState.Untested;
+            ParentNode = null;
+        }
+    }
+
+
+
+    public enum NodeState { Untested, Open, Closed }
+
+    
+}
