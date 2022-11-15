991f3bf24f0f6197e571dbffe46ea67cba19223f
OlivierPoitras <poitrasolivier@gmail.com>
2022-11-15 14:28:42 -0500

    Fixed code

--- Bot.cs
+++ Bot.cs
@@ -25,7 +25,7 @@ namespace Application
         }
 
         /// <summary>
-        /// Here is where the magic happens, for now the moves are random. I bet you can do better ;)
+        /// Handle the turn here, returning a move
         /// </summary>
         public Move GetNextMove(Tick tick)
         {
@@ -36,43 +36,17 @@ namespace Application
             else
             {
                 Position closestPort = FindClosestPort(tick);
-                Console.WriteLine("Current tick: " + tick.CurrentTick);
-                Console.WriteLine($"x : {closestPort.Column} et y : {closestPort.Row}");
+                //Console.WriteLine("Current tick: " + tick.CurrentTick);
+                //Console.WriteLine($"x : {closestPort.Column} et y : {closestPort.Row}");
                 return FindNextAction(tick, closestPort);
             }
         }
-
-        /*private Map NewMap(Map map)
-        {
-            List<List<int>> new_matrix = new List<List<int>()>();
-            foreach(List<int> colonne in map.Topology)
-            {
-                List<int> new_list = new List<int>();
-                foreach(int elements in colonne)
-                {
-                    //Il faut changer n selon la hauteur du niveau d'eau
-                    if (elements - n) >= 0
-                    {
-                        new_list.Add(0);
-                    }
-                    else
-                    {
-                        new_list.Add(1);
-                    }            
-                }
-                new_matrix.Add(new_list)
-            }
-            return new_matrix;
-        }*/
         
-
-        private LinkedListNode GetNodeAtIndex(int index, LinkedList<CoordinatedDirections> list) {
-            LinkedListNode currentNode = list.First;
-            for (int i = 0; i < index; i++) {
-                if (list.IndexOf(currentNode) == index)
-                    return currentNode;
-                else currentNode = currentNode.Next ?? list.First;
-            }
+        private LinkedListNode<CoordinatedDirections> GetNodeAtIndex(int index, LinkedList<CoordinatedDirections> list) {
+            LinkedListNode<CoordinatedDirections> node = list.First;
+            for (int i = 0; i < index; i++)
+                node = node.Next;
+            return node;
         }
 
         private string VerifyMove(int n, Tick tick)
@@ -89,45 +63,15 @@ namespace Application
 
             LinkedListNode<CoordinatedDirections> currentDirection = GetNodeAtIndex(n, directions);
 
-            // currentDirection = currentDirection.Next ?? directions.First;
-
-
-            // List<string> ListDirection = new List<string>(){
-            //     "E", "SE", "S", "SW", "W", "NW", "N", "NE"
-            // };
-            
-            /*List<List<int>> ListCoord = new List<List<int>>();
-            ListCoord.Add(new List<int> { 0, 1});
-            ListCoord.Add(new List<int> { 1, 1});
-            ListCoord.Add(new List<int> { 1, 0});
-            ListCoord.Add(new List<int> { 1, -1});
-            ListCoord.Add(new List<int> { 0, -1});
-            ListCoord.Add(new List<int> { -1, -1});
-            ListCoord.Add(new List<int> { -1, 0});
-            ListCoord.Add(new List<int> { -1, 1});*/
-
             for (int i = 0; i < 8; i++) {
-                if (tick.Map.Topology[tick.CurrentLocation.Row + currentDirection.Value.Y][tick.CurrentLocation.Column + currentDirection.Value.X] < tick.TideSchedule[tick.CurrentTick]){
+                if (tick.Map.Topology[tick.CurrentLocation.Row + currentDirection.Value.Y][tick.CurrentLocation.Column + currentDirection.Value.X] < tick.Map.TideLevels.Min)
+                {
                     return currentDirection.Value.Direction;
                 }
                 currentDirection = currentDirection.Next ?? directions.First;
             }
  
-            // for(int i = 0; i < 8; i++)
-            // {
-            //     Position nextPosition = new Position();
-            //     nextPosition.Row = tick.CurrentLocation.Row + currentDirection.Value.Y;
-            //     nextPosition.Column = tick.CurrentLocation.Column + currentDirection.Value.X;
-            //     if ((tick.Map.Topology[nextPosition.Row][nextPosition.Column] - tick.TideSchedule[tick.CurrentTick]) < 0)
-            //     {
-            //         return currentDirection;
-            //     }
-            //     else
-            //     {
-            //         currentDirection = currentDirection.Next ?? directions.First;
-            //     }
-            // }
-            return "E"; // TODO: on est bloqué, direction arbitraire
+            return "E"; // TODO: on est bloqué, direction arbitraire. On devrait mieux gerer, au lieu de retourner "E"
         }
 
         private Position FindClosestPort(Tick tick)
@@ -146,7 +90,7 @@ namespace Application
             Position closestPort = new Position();
             double closestDistance = 0;
 
-            Console.WriteLine($"x : {tick.CurrentLocation.Column} et y : {tick.CurrentLocation.Row}");
+            //Console.WriteLine($"x : {tick.CurrentLocation.Column} et y : {tick.CurrentLocation.Row}");
 
             foreach(var direction in ports)
             {
